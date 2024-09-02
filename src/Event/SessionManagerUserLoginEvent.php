d368d8a58fa641c8c7ca9f25393e81cf86de195a
Miķelis Zaļais <mik@prog.lv>
2024-09-02 12:54:10 +0300

    Update SessionManagerUserLoginEvent.php
    D10 compat

--- src/Event/SessionManagerUserLoginEvent.php
+++ src/Event/SessionManagerUserLoginEvent.php
@@ -3,7 +3,7 @@
 namespace Drupal\session_manager\Event;
 
 use Drupal\user\UserInterface;
-use Symfony\Component\EventDispatcher\Event;
+use Drupal\Component\EventDispatcher\Event;
 
 /**
  * Event that is fired when a user logs in.
